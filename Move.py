22edd7860da5668d8ae922c7f43858844c9725fb
L <60981745+pankourgiou@users.noreply.github.com>
2025-05-20 21:57:40 +0300

    Update Move.py

--- Move.py
+++ Move.py
@@ -1,7 +1,7 @@
 #Rolvin means to unite
-for _ in range(100):
+for _ in range(1000):
   print("googleplex/2*googleplex = 1/2")
 #Rolvin 100 results of the function
-print("五十")
+print("五百")
 for _ in range(1000):
   print("In varietate concordia")
